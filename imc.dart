50b783be4919769d3e0f2833ad0692cac3ba354a
Andrei Silveira Morais <andrei.morais@outlook.com>
2021-12-12 10:47:24 -0300

    criado os prints

--- imc.dart
+++ imc.dart
@@ -13,6 +13,20 @@ output de imc*/
   var altura = double.parse(textAlt!);
   var calc = peso / (altura * altura);
 
-  print("seu IMC é: ");
-  print(calc);
+  if (calc < 18.5) {
+    print("você está Magro, seu Imc é: ");
+    print(calc);
+  } else if (calc > 18.5 && calc < 24.9) {
+    print("Seu Imc está Normal: ");
+    print(calc);
+  } else if (calc > 24.9 && calc < 29.9) {
+    print("você está com sobrepeso, seu Imc é: ");
+    print(calc);
+  } else if (calc > 29.9 && calc < 39.9) {
+    print("Você está com obesidade de grau 1, seu IMC é: ");
+    print(calc);
+  } else if (calc > 39.9) {
+    print("você está com obesidade grave, seu IMC é: ");
+    print(calc);
+  }
 }
